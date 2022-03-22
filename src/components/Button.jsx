a922378439a7cd6431be03474e35bb5b16e499db
shashifw11 <shashi_fw11_220@masai.school>
2022-03-22 20:50:49 +0530

    new

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,10 @@ import styled from "styled-components"
        margin-left : 5px ; 
        padding : 5px ; 
        background : ${props => props.id1 ? "skyblue" : "white"};
-       color: ${props => props.id5 ? "skyblue" : "black"};
-       border : ${props=>props.id2 ? "1px solid gray" : "1px dashed black"};
+       color: ${props => props.id1 ? "white" : props.id5 ? "skyblue" : "black"};
+       border : ${props => props.id2 ? "1px solid gray" : props.id3 ? "1px dashed black" : "none"};
        border-radius : 5px ; 
-   `
+       font-size : 20px ; 
+  `
 
 
- 
